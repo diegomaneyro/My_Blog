87bdb0de79eccdb0ec04e03b39003a749ce28790
diego maneyro <diegomaneyro@gmail.com>
2025-02-03 02:17:51 -0300

    update project

--- blog/settings.py
+++ blog/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'manage_post.apps.ManagePostConfig',
+    'user.apps.UserConfig',
 ]
 
 MIDDLEWARE = [
--- user/views.py
+++ user/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render 
+from django.http import HttpResponse, request
 
 # Create your views here.
+def Login(request):
+    return HttpResponse('Login')
+
+def Register(request):
+    return HttpResponse('Register')
