ade860e24ea0558db689c2572ae3e94433ba73ac
diego maneyro <diegomaneyro@gmail.com>
2025-02-17 12:23:07 -0300

    Update project

--- blog/settings.py
+++ blog/settings.py
@@ -133,3 +133,7 @@ MEDIA_ROOT = BASE_DIR / 'media'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# redireccionamiento de url
+LOGIN_REDIRECT_URL = 'index'
+LOGOUT_REDIRECT_URL = 'login'
